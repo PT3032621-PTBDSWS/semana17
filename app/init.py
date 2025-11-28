cebea8204427ef34d4be025f2bea7a65757278fa
Gustavo Maximo Da Silva <gmaximodasilva@gmail.com>
2025-11-28 19:54:32 -0300

    Update and rename __init__.py to init.py

--- app/init.py
+++ app/init.py
@@ -1,26 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
 from config import Config
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
 
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
-    app.config.from_object(config_class)
+    app.config.from_object(Config)
 
     db.init_app(app)
     bootstrap.init_app(app)
-    login_manager.init_app(app)
-
-    from .auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from .main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    from app.main.views import page_not_found, internal_error
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, internal_error)
 
     return app
